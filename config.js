fc2be89295eaed1f20a538df5e2fec710a53baa8
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-04-10 00:03:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 const toBool = (x) => x === 'true';
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'inrl~mF9RCwRF',
+    SESSION_ID: process.env.SESSION_ID || 'Devilser~AHs',
     HANDLERS: process.env.HANDLERS || '.',//[.]
-    BOT_NAME: process.env.BOT_NAME || 'the-enrl',
+    BOT_NAME: process.env.BOT_NAME || 'DevilSer_Md',
     OWNER_NAME: process.env.OWNER_NAME || '🙃',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '919074692450',
-    SUDO: process.env.SUDO || '919074692450,917025099154',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '916282378078',
+    SUDO: process.env.SUDO || '916282378078',
     MODE: (process.env.MODE || 'public').trim(),
     STICKER_DATA: process.env.STICKER_DATA || '🎯inrl-𝙼𝙳;loki-x☘️',
 };
