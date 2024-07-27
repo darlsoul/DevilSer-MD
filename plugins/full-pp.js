bed4de5202e1c57dcd0fbee5bcadfe290eddcc95
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-07-27 16:23:55 +0530

    Update full-pp.js

--- plugins/full-pp.js
+++ plugins/full-pp.js
@@ -1,43 +1,10 @@
-const {pnix, mode} = require('../lib');
+const { pnix, mode} = require("../lib");
+
 pnix({
-    pattern: "pp",
+    pattern: "hi",
     type: "main",
-    desc: "Set bot Dp",
+    desc: "Bot response.",
     fromMe: mode
-}, async(msg, client) => {
-    if(!m.quoted) return await msg.send("Reply to a image");
-       let img = await msg.quoted.download('./image.jpg');
-       updateProfilePicture("918086828461@s.whatsapp.net", img, client);
-       await msg.send("_Profile Picture Updated_");
-});
-
-async function updateProfilePicture(jid, imag, client) {
-  const { query } = client;
-  const { img } = await generateProfilePicture(imag);
-  await query({
-    tag: "iq",
-    attrs: {
-      to: jid,
-      type: "set",
-      xmlns: "w:profile:picture",
-    },
-    content: [
-      {
-        tag: "picture",
-        attrs: { type: "image" },
-        content: img,
-      },
-    ],
-  });
-}
-
-async function generateProfilePicture(buffer) {
-  const jimp = await Jimp.read(buffer);
-  const min = jimp.getWidth();
-  const max = jimp.getHeight();
-  const cropped = jimp.crop(0, 0, min, max);
-  return {
-    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
-    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
-  };
-}
+}, async(msg, match) => {
+        await msg.reply(`Hello`);
+})
