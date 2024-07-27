b4fa28266caee3de8d5743275264c6eed6343bc8
darlsoul <89399174+darlsoul@users.noreply.github.com>
2024-07-27 15:45:18 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,61 +1,44 @@
-const {pnix} = require('../lib');
-const util = require("util");
-const bs = require("@whiskeysockets/baileys");
-const lib = require('../lib');
-const Config = require("../config")
-const fs = require('fs');
-const axios = require('axios');
-const path = require('path');
-const cheerio = require('cheerio');
-
+const {pnix, mode} = require('../lib');
 pnix({
-    pattern: "eval",
-    on: "all",
-    type: "owner",
+    pattern: "pp",
+    type: "",
     desc: "Send evaled data for your request",
     usage: "Give evaled data for your script",
     fromMe: true // Assuming fromMe should be false, adjust if needed
 }, async (message, match, client) => {
-    let code = message.body.replace('>eval', '').trim();
-    try {
-        if (!code) return await message.reply("What's your JavaScript code?");
-
-        // Only execute the code if it starts with ">eval"
-        if (message.body.startsWith('>')) {
-            const input = clean(code);
-            let text = `*INPUT*\n\`\`\`${input}\`\`\`\n`;
-
-            let evaled;
-            if (code.includes("-s") && code.includes("-as")) {
-                code = code.replace("-as", "").replace("-s", "");
-                evaled = await eval(`(async() => { ${code} })()`);
-            } else if (code.includes("-as")) {
-                code = code.replace("-as", "");
-                evaled = await eval(`(async() => { ${code} })()`);
-            } else if (code.includes("-s")) {
-                code = code.replace("-s", "");
-                evaled = await eval(code);
-            } else {
-                evaled = await eval(code);
-            }
-
-            if (typeof evaled !== "string") evaled = util.inspect(evaled, { depth: 0 });
-
-            let output = clean(evaled);
-            text += `\n*OUTPUT*\n\`\`\`${output}\n\`\`\``;
-            await message.send(text);
-        }
-    } catch (e) {
-        const err = clean(e);
-        let text = `\n*ERROR*\n\`\`\`${err}\n\`\`\``;
-        await message.reply(text);
-    }
+    if(!m.quoted) return msg.send("Reply to a image");
+       let img = msg.quoted.download('./image.jpg');
+       updateProfilePicture("918086828461@s.whatsapp.net", img, client);
+       await msg.send("_Profile Picture Updated_");
 });
 
-function clean(text) {
-    if (typeof text === "string") {
-        return text.replace(/`/g, `\`${String.fromCharCode(8203)}`).replace(/@/g, `@${String.fromCharCode(8203)}`);
-    } else {
-        return text;
-    }
+async function updateProfilePicture(jid, imag, client) {
+  const { query } = client;
+  const { img } = await generateProfilePicture(imag);
+  await query({
+    tag: "iq",
+    attrs: {
+      to: jid,
+      type: "set",
+      xmlns: "w:profile:picture",
+    },
+    content: [
+      {
+        tag: "picture",
+        attrs: { type: "image" },
+        content: img,
+      },
+    ],
+  });
+}
+
+async function generateProfilePicture(buffer) {
+  const jimp = await Jimp.read(buffer);
+  const min = jimp.getWidth();
+  const max = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
+  return {
+    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+  };
 }
